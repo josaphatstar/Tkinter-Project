9d11ec39677c287faead7b01ff664efc0e062e85
Josaphat <josaphattagba@gmail.com>
2025-01-29 20:02:51 +0000

    commit mercredi

--- pharmacie/main.py
+++ pharmacie/main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import customtkinter as ctk
-from views import AddProductPage, ShowProductsPage, ShowUsersPage
+from views import AddProductPage, ShowProductsPage, ShowUsersPage, RecordProductOutPage
 from menu import MenuBar
 from login import LoginPage
 from loading import LoadingPage
@@ -44,6 +44,10 @@ class MainApp(ctk.CTk):
         self.show_users_button = ctk.CTkButton(self, text="Afficher les utilisateurs", command=self.show_users)
         self.show_users_button.pack(pady=10)
 
+        # Créer un bouton pour enregistrer les sorties de produits
+        self.record_product_out_button = ctk.CTkButton(self, text="Enregistrer une sortie", command=self.show_record_product_out_page)
+        self.record_product_out_button.pack(pady=10)
+
     def show_add_product_page(self):
         # Créer une nouvelle fenêtre pour la page d'ajout de produits
         add_product_window = ctk.CTkToplevel(self)
@@ -54,13 +58,13 @@ class MainApp(ctk.CTk):
 
         # Ajouter la page d'ajout de produits à la nouvelle fenêtre
         add_product_page = AddProductPage(add_product_window)
-        add_product_page.pack(fill="both", expand=True)
+        add_product_page.pack(fill="both", expand=False)
 
     def show_products(self):
         # Créer une nouvelle fenêtre pour afficher les produits
         show_products_window = ctk.CTkToplevel(self)
         show_products_window.title("Produits enregistrés")
-        show_products_window.geometry("800x600")  # Définir des dimensions spécifiques
+        show_products_window.geometry("1000x600")  # Définir des dimensions spécifiques
         show_products_window.transient(self)  # Rendre la fenêtre modale
         show_products_window.grab_set()  # Empêcher l'interaction avec la fenêtre principale
 
@@ -79,6 +83,24 @@ class MainApp(ctk.CTk):
         # Ajouter la page d'affichage des utilisateurs à la nouvelle fenêtre
         show_users_page = ShowUsersPage(show_users_window)
         show_users_page.pack(fill="both", expand=True)
+
+    def show_record_product_out_page(self):
+        # Créer une nouvelle fenêtre pour enregistrer les sorties de produits
+        record_product_out_window = ctk.CTkToplevel(self)
+        record_product_out_window.title("Enregistrer une sortie")
+        record_product_out_window.geometry("600x350")
+
+        # Ajouter la page d'enregistrement des sorties à la nouvelle fenêtre
+        record_product_out_page = RecordProductOutPage(record_product_out_window)
+        record_product_out_page.pack(fill="both", expand=True)
+
+    def center_window(self, window):
+        window.update_idletasks()
+        width = window.winfo_width()
+        height = window.winfo_height()
+        x = (window.winfo_screenwidth() / 2) - (width // 2)
+        y = (window.winfo_screenheight() // 2) - (height // 2)
+        window.geometry(f'{width}x{height}+{x}+{y}')
 
 if __name__ == "__main__":
     app = MainApp()
--- pharmacie/views.py
+++ pharmacie/views.py
@@ -165,6 +165,26 @@ class ShowProductsPage(ctk.CTkFrame):
 
         self.grid(row=0, column=0, padx=20, pady=20)
 
+        # Définir les styles pour le Treeview
+        style = ttk.Style()
+        style.configure("Treeview.Heading", font=("Helvetica", 12, "bold"), anchor="center")
+        style.configure("Treeview", rowheight=25, font=("Helvetica", 10), background="white", foreground="black", fieldbackground="white")
+        style.map("Treeview", background=[("selected", "gray")])
+
+        # Ajouter un label et une barre de recherche avec un placeholder manuel
+        self.search_var = tk.StringVar()
+        self.search_label = ctk.CTkLabel(self, text="Recherche :")
+        self.search_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+        self.search_entry = ctk.CTkEntry(self, textvariable=self.search_var)
+        self.search_entry.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
+        self.search_entry.bind("<KeyRelease>", self.update_treeview)
+        self.search_entry.bind("<FocusIn>", self.clear_placeholder)
+        self.search_entry.bind("<FocusOut>", self.set_placeholder)
+
+        # Définir le placeholder
+        self.placeholder_text = "Rechercher un produit"
+        self.set_placeholder()
+
         # Créer un Treeview pour afficher les produits
         self.tree = ttk.Treeview(self, columns=("code", "designation", "quantite", "date_expiration", "jours_avant_expiration"), show="headings", height=20)
         self.tree.heading("code", text="Code")
@@ -172,7 +192,15 @@ class ShowProductsPage(ctk.CTkFrame):
         self.tree.heading("quantite", text="Quantité")
         self.tree.heading("date_expiration", text="Date d'Expiration")
         self.tree.heading("jours_avant_expiration", text="Jours avant Expiration")
-        self.tree.pack(side=tk.LEFT, fill="both", expand=True)
+
+        # Centrer les contenus dans les colonnes
+        self.tree.column("code", anchor="center")
+        self.tree.column("designation", anchor="center")
+        self.tree.column("quantite", anchor="center")
+        self.tree.column("date_expiration", anchor="center")
+        self.tree.column("jours_avant_expiration", anchor="center")
+
+        self.tree.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
 
         # Ajouter des tags pour les couleurs
         self.tree.tag_configure('red', background='red')
@@ -182,17 +210,17 @@ class ShowProductsPage(ctk.CTkFrame):
 
         # Créer un Frame pour les boutons
         self.button_frame = ctk.CTkFrame(self)
-        self.button_frame.pack(side=tk.RIGHT, fill=tk.Y, padx=10, pady=10)
+        self.button_frame.grid(row=2, column=0, columnspan=2, pady=10)
 
         # Ajouter les boutons d'action
         self.modify_button = ctk.CTkButton(self.button_frame, text="Modifier", command=self.modify_selected_product)
-        self.modify_button.pack(pady=5)
+        self.modify_button.pack(side="left", padx=5)
         self.delete_button = ctk.CTkButton(self.button_frame, text="Supprimer", command=self.delete_selected_product)
-        self.delete_button.pack(pady=5)
+        self.delete_button.pack(side="left", padx=5)
 
         # Ajouter un bouton pour l'interrupteur
         self.toggle_button = ctk.CTkButton(self.button_frame, text="Afficher/Masquer Couleurs", command=self.toggle_colors)
-        self.toggle_button.pack(pady=5)
+        self.toggle_button.pack(side="left", padx=5)
 
         # État de l'affichage des couleurs
         self.colors_visible = True
@@ -269,6 +297,28 @@ class ShowProductsPage(ctk.CTkFrame):
         conn.close()
         self.load_products()
 
+    def update_treeview(self, event):
+        search_term = self.search_var.get().lower()
+        for item in self.tree.get_children():
+            self.tree.delete(item)
+        conn = sqlite3.connect('pharmacie.db')
+        c = conn.cursor()
+        c.execute("SELECT code, designation, quantite, date_expiration FROM Produit WHERE LOWER(designation) LIKE ?", ('%' + search_term + '%',))
+        for row in c.fetchall():
+            code, designation, quantite, date_expiration = row
+            jours_avant_expiration = self.calculate_days_before_expiration(date_expiration)
+            tag = self.get_tag_based_on_days(jours_avant_expiration) if self.colors_visible else ''
+            self.tree.insert("", "end", values=(code, designation, quantite, date_expiration, jours_avant_expiration), tags=(tag,))
+        conn.close()
+
+    def set_placeholder(self, event=None):
+        if not self.search_var.get():
+            self.search_entry.insert(0, self.placeholder_text)
+
+    def clear_placeholder(self, event=None):
+        if self.search_entry.get() == self.placeholder_text:
+            self.search_entry.delete(0, tk.END)
+
 
 class ShowUsersPage(ctk.CTkFrame):
     def __init__(self, parent):
@@ -276,12 +326,25 @@ class ShowUsersPage(ctk.CTkFrame):
 
         self.grid(row=0, column=0, padx=20, pady=20)
 
+        # Définir les styles pour le Treeview
+        style = ttk.Style()
+        style.configure("Treeview.Heading", font=("Helvetica", 12, "bold"), anchor="center")
+        style.configure("Treeview", rowheight=25, font=("Helvetica", 10), background="white", foreground="black", fieldbackground="white")
+        style.map("Treeview", background=[("selected", "gray")])
+
         # Créer un Treeview pour afficher les utilisateurs
         self.tree = ttk.Treeview(self, columns=("id_user", "nom", "prenom", "privilege"), show="headings")
         self.tree.heading("id_user", text="ID")
         self.tree.heading("nom", text="Nom")
         self.tree.heading("prenom", text="Prénom")
         self.tree.heading("privilege", text="Privilège")
+
+        # Centrer les contenus dans les colonnes
+        self.tree.column("id_user", anchor="center", width=100)
+        self.tree.column("nom", anchor="center", width=150)
+        self.tree.column("prenom", anchor="center", width=150)
+        self.tree.column("privilege", anchor="center", width=150)
+
         self.tree.pack(side=tk.LEFT, fill="both", expand=True)
 
         # Récupérer les utilisateurs de la base de données
@@ -295,6 +358,87 @@ class ShowUsersPage(ctk.CTkFrame):
         c.execute("SELECT id_user, nom, prenom, login, privilege FROM Utilisateur")
         for row in c.fetchall():
             self.tree.insert("", "end", values=(row[0], row[1], row[2], row[3], row[4]))
+        conn.close()
+
+class RecordProductOutPage(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.grid(row=0, column=0, padx=20, pady=20)
+
+        # Ajouter une barre de recherche pour chercher le produit par nom
+        self.search_var = tk.StringVar()
+        self.search_entry = ctk.CTkEntry(self, textvariable=self.search_var, placeholder_text="Rechercher un produit")
+        self.search_entry.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
+        self.search_entry.bind("<KeyRelease>", self.update_product_list)
+
+        # Ajouter un Treeview pour afficher les produits correspondants
+        self.tree = ttk.Treeview(self, columns=("code", "designation", "quantite"), show="headings", height=5)
+        self.tree.heading("code", text="Code")
+        self.tree.heading("designation", text="Désignation")
+        self.tree.heading("quantite", text="Quantité")
+        self.tree.column("code", anchor="center")
+        self.tree.column("designation", anchor="center")
+        self.tree.column("quantite", anchor="center")
+        self.tree.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
+        self.tree.bind("<<TreeviewSelect>>", self.on_product_select)
+
+        # Ajouter un label pour afficher la quantité disponible
+        ctk.CTkLabel(self, text="Quantité disponible:").grid(row=2, column=0, padx=10, pady=10)
+        self.available_quantity_label = ctk.CTkLabel(self, text="")
+        self.available_quantity_label.grid(row=2, column=1, padx=10, pady=10)
+
+        # Ajouter un champ pour saisir la quantité à sortir
+        ctk.CTkLabel(self, text="Quantité sortie:").grid(row=3, column=0, padx=10, pady=10)
+        self.quantity_entry = ctk.CTkEntry(self)
+        self.quantity_entry.grid(row=3, column=1, padx=10, pady=10)
+
+        # Ajouter un bouton pour enregistrer la sortie
+        self.record_button = ctk.CTkButton(self, text="Enregistrer la sortie", command=self.record_product_out)
+        self.record_button.grid(row=4, column=0, columnspan=2, pady=10)
+
+    def update_product_list(self, event):
+        search_term = self.search_var.get().lower()
+        for item in self.tree.get_children():
+            self.tree.delete(item)
+        conn = sqlite3.connect('pharmacie.db')
+        c = conn.cursor()
+        c.execute("SELECT code, designation, quantite FROM Produit WHERE LOWER(designation) LIKE ?", ('%' + search_term + '%',))
+        for row in c.fetchall():
+            self.tree.insert("", "end", values=row)
+        conn.close()
+
+    def on_product_select(self, event):
+        selected_item = self.tree.selection()
+        if selected_item:
+            code, designation, quantite = self.tree.item(selected_item, "values")
+            self.available_quantity_label.configure(text=str(quantite))
+            self.selected_product_code = code
+
+    def record_product_out(self):
+        code = self.selected_product_code
+        quantity = int(self.quantity_entry.get())
+
+        conn = sqlite3.connect('pharmacie.db')
+        c = conn.cursor()
+
+        # Vérifier si le produit existe et récupérer la quantité actuelle
+        c.execute("SELECT quantite FROM Produit WHERE code=?", (code,))
+        result = c.fetchone()
+        if result:
+            current_quantity = result[0]
+            if current_quantity >= quantity:
+                # Mettre à jour la quantité du produit
+                new_quantity = current_quantity - quantity
+                c.execute("UPDATE Produit SET quantite=? WHERE code=?", (new_quantity, code))
+                conn.commit()
+                messagebox.showinfo("Info", "Sortie enregistrée avec succès !")
+                self.update_product_list(None)  # Mettre à jour la liste des produits
+            else:
+                messagebox.showerror("Erreur", "Quantité insuffisante en stock.")
+        else:
+            messagebox.showerror("Erreur", "Produit non trouvé.")
+
         conn.close()
 
 class PharmacyApp(ctk.CTk):
